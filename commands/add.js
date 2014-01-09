bd0a23606cd98c791cedc16d23a1f49b3d67748b
Mathias Buus <mathiasbuus@gmail.com>
2014-01-09 22:46:45 +0100

    env support

--- commands/add.js
+++ commands/add.js
@@ -1,8 +1,11 @@
 var client = require('../');
 var ui = require('../lib/ui');
+var parse = require('../lib/parse-env');
 
 module.exports = function(id, opts) {
 	if (!id) return ui.error('Service name required');
+
+	if (opts.env) opts.env = parse(opts.env);
 
 	var c = client(opts.remote);
 	var unspin = ui.spin('Adding', id);
--- commands/update.js
+++ commands/update.js
@@ -1,10 +1,27 @@
+var xtend = require('xtend');
 var client = require('../');
 var ui = require('../lib/ui');
+var parse = require('../lib/parse-env');
 
 module.exports = function(id, opts) {
 	if (!id) return ui.error('Service name required');
 
+	if (opts.env) opts.env = parse(opts.env);
+
 	var c = client(opts.remote);
 	var unspin = ui.spin('Updating', id);
-	c.update(id, opts, unspin);
+
+	var done = function(err) {
+		if (err) return unspin(err);
+		c.update(id, opts, unspin);
+	};
+
+	var envAdd = opts['env-add'] && parse(opts['env-add']);
+	if (!envAdd) return done();
+
+	c.get(id, function(err, service) {
+		if (err) return done(err);
+		opts.env = xtend(service.env, envAdd);
+		done();
+	});
 };
