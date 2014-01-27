60cba24a7cee43d96ce187d7630cb35cacec9628
Mathias Buus <mathiasbuus@gmail.com>
2014-01-27 15:53:30 +0100

    fixed dead code ref

--- commands/sync.js
+++ commands/sync.js
@@ -1,6 +1,5 @@
 var client = require('../');
 var ui = require('../lib/ui');
-var parse = require('../lib/parse-env');
 
 module.exports = function(remote, id, opts) {
 	if (!id) return ui.error('Service name required');
