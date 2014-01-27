f05e5dc4d9360518aa4a62f06688afca705fea03
Mathias Buus <mathiasbuus@gmail.com>
2014-01-27 15:20:28 +0100

    new signature

--- bin/hms.js
+++ bin/hms.js
@@ -1,26 +1,17 @@
 #!/usr/bin/env node
 
 var tab = require('tabalot');
+var rm = require('../lib/remotes');
 var os = require('os');
-var fs = require('fs');
-var xtend = require('xtend');
-var config = require('../lib/config');
-var noop = [];
 
-var ids = function(word, opts, cb) {
-	var hms = require('../');
-	var c = hms(opts.remote);
-	var cached = config.cache(opts.remote);
+var remotes = function(word, opts, cb) {
+	if (word.indexOf('@') > -1) return cb(null, '@host');
+	cb(null, rm.list());
+};
 
-	if (cached) return cb(null, cached);
-
-	c.list(function(err, list) {
-		if (err) return cb(err);
-		list = list.map(function(service) {
-			return service.id;
-		});
-		cb(null, config.cache(opts.remote, list));
-	});
+var resolve = function(remote, opts) {
+	if (!remote) return require('../lib/ui').error('Remote is required');
+	return require('xtend')(rm.read(remote) || {url:remote}, opts);
 };
 
 var help = function() {
@@ -28,43 +19,63 @@ var help = function() {
 	process.exit(1);
 };
 
-var defaults = function(opts) {
-	var ui = require('../lib/ui');
-	var conf = config.read();
-	var r = opts.remote;
-	conf.fingerprint = conf.fingerprints ? conf.fingerprints[r] : null;
-	opts = xtend(conf, opts);
-	if (opts.force) return opts;
-	if (r && r.indexOf('@') > -1 && !opts.fingerprint) return ui.error('Host is not verified. Run the verify command');
-	return opts;
+var ids = function(word, opts, cb) {
+	var client = require('../');
+	var name = opts._[1];
+
+	var c = client(resolve(opts._[1], opts));
+	var cached = rm.cache(name, 'ids');
+
+	if (cached) return cb(null, cached);
+
+	c.list(function(err, list) {
+		if (err) return cb(err);
+
+		list = list.map(function(service) {
+			return service.id;
+		});
+
+		cb(null, rm.cache(name, 'ids', list));
+	});
 };
 
 tab('*')
-	('--key', '-i', '-k', '@file')
-	('--passphrase')
-	('--fingerprint')
 	('--force', '-f')
-	('--remote', '-r', '@host');
+	('--key', '-i', '-k', '@file')
+	('--passphrase');
 
-tab('list')
-	(ids)
-	(function(id, opts) {
-		require('../commands/list')(id, defaults(opts));
+tab('remotes')
+	('--yes', '-y')
+	('--fingerprint')
+	('--no-fingerprint')
+	(['add', 'remove', 'list'])
+	(remotes)
+	('@host')
+	(function(cmd, remote, host, opts) {
+		require('../commands/remotes')(cmd, remote, host, opts);
 	});
 
 tab('add')
-	(ids)
 	('--start', '-s')
 	('--build', '-b')
 	('--docks', '-d')
 	('--env', '-e')
-	(function(id, opts) {
-		config.cache(opts.remote, null);
-		require('../commands/add')(id, defaults(opts));
+	(remotes)
+	(ids)
+	(function(remote, id, opts) {
+		rm.cache(remote, 'ids', null);
+		require('../commands/add')(resolve(remote, opts), id, opts);
+	});
+
+tab('remove')
+	(remotes)
+	(ids)
+	(function(remote, id, opts) {
+		rm.cache(remote, 'ids', null);
+		require('../commands/remove')(resolve(remote, opts), id, opts);
 	});
 
 tab('update')
-	(ids)
 	('--start', '-s')
 	('--build', '-b')
 	('--docks', '-d')
@@ -74,85 +85,89 @@ tab('update')
 	('--no-build')
 	('--no-docks')
 	('--no-env')
-	(function(id, opts) {
-		config.cache(opts.remote, null);
-		require('../commands/update')(id, defaults(opts));
+	(remotes)
+	(ids)
+	(function(remote, id, opts) {
+		require('../commands/update')(resolve(remote, opts), id, opts);
 	});
 
-tab('remove')
+tab('list')
+	(remotes)
 	(ids)
-	(function(id, opts) {
-		config.cache(opts.remote, null);
-		require('../commands/remove')(id, defaults(opts));
-	});
-
-tab('start')
-	(ids)
-	(function(id, opts) {
-		require('../commands/start')(id, defaults(opts));
-	});
-
-tab('restart')
-	(ids)
-	(function(id, opts) {
-		require('../commands/restart')(id, defaults(opts));
-	});
-
-tab('stop')
-	(ids)
-	(function(id, opts) {
-		require('../commands/stop')(id, defaults(opts));
+	(function(remote, id, opts) {
+		require('../commands/list')(resolve(remote, opts), id, opts);
 	});
 
 tab('ps')
-	(ids)
 	('--env', '-e')
-	(function(id, opts) {
-		require('../commands/ps')(id, defaults(opts));
+	(remotes)
+	(ids)
+	(function(remote, id, opts) {
+		require('../commands/ps')(resolve(remote, opts), id, opts);
+	});
+
+tab('start')
+	(remotes)
+	(ids)
+	(function(remote, id, opts) {
+		require('../commands/start')(resolve(remote, opts), id, opts);
+	});
+
+tab('stop')
+	(remotes)
+	(ids)
+	(function(remote, id, opts) {
+		require('../commands/stop')(resolve(remote, opts), id, opts);
+	});
+
+tab('restart')
+	(remotes)
+	(ids)
+	(function(remote, id, opts) {
+		require('../commands/restart')(resolve(remote, opts), id, opts);
 	});
 
 tab('log')
-	(ids)
 	('--no-events')
 	('--no-id')
 	('--no-origin')
-	(function(id, opts) {
-		require('../commands/log')(id, defaults(opts));
-	});
-
-tab('deploy')
+	(remotes)
 	(ids)
-	('--revision')
-	(function(id, opts) {
-		require('../commands/deploy')(id, defaults(opts));
+	(function(remote, id, opts) {
+		require('../commands/log')(resolve(remote, opts), id, opts);
 	});
 
 tab('sync')
-	(ids)
 	('--restart')
-	(function(id, opts) {
-		require('../commands/sync')(id, defaults(opts));
+	(remotes)
+	(ids)
+	(function(remote, id, opts) {
+		require('../commands/sync')(resolve(remote, opts), id, opts);
 	});
 
-tab('verify')
-	('--expect', '-e')
-	(function(opts) {
-		require('../commands/verify')(opts);
+tab('deploy')
+	('--revision', '-r')
+	(remotes)
+	(ids)
+	(function(remote, id, opts) {
+		require('../commands/deploy')(resolve(remote, opts), id, opts);
 	});
 
 tab('tarball')
-	(ids)
 	('--out', '-o', '@file')
-	(function(id, opts) {
-		require('../commands/tarball')(id, defaults(opts));
+	(remotes)
+	(ids)
+	(function(remote, id, opts) {
+		require('../commands/tarball')(resolve(remote, opts), id, opts);
 	});
 
 tab('dock')
+	(remotes)
 	('--id', '-i', os.hostname())
 	('--port', '-p', 10002)
 	('--db', '@file')
 	(function(opts) {
-		require('../commands/dock')(defaults(opts));
+		require('../commands/dock')(resolve(remote, opts), opts);
 	});
 
 tab('terminal')
@@ -160,7 +175,7 @@ tab('terminal')
 	('--dock', '-d')
 	('--db', '@file')
 	(function(opts) {
-		require('../commands/terminal')(defaults(opts));
+		require('../commands/terminal')(opts);
 	});
 
 tab()
