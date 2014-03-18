dd071be21d831398475bfdd7c7779546bc85092f
Eduardo Sorribas <eduardo@sorribas.org>
2014-03-18 22:14:39 +0100

    Forward log in the 'start' and 'restart' commands

--- commands/start.js
+++ commands/start.js
@@ -1,10 +1,21 @@
 var client = require('../');
 var ui = require('../lib/ui');
+var logStream = require('../lib/log-stream');
 
 module.exports = function(remote, id, opts) {
 	if (!id) return ui.error('Service name required');
 
 	var c = client(remote);
+
+	c.subscribe(id, function(err) {
+		if (err) return ui.error(err);
+	});
+
 	var unspin = ui.spin('Starting', id);
-	c.start(id, unspin);
+	c.start(id, function(err) {
+		unspin(err);
+
+		console.log('\nForwarding', id, 'output\n');
+		logStream(c).pipe(process.stdout);
+	});
 };
