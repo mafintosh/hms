851a9bbdbc42f2daa601bc55106e5d5f6c94e3d5
Martin Gausby <martin@gausby.dk>
2015-01-28 16:05:58 +0100

    redid the indentation

--- commands/status.js
+++ commands/status.js
@@ -3,28 +3,28 @@ var client = require('../');
 var ui = require('../lib/ui');
 
 module.exports = function(remote, id, opts) {
-	var c = client(remote);
+  var c = client(remote);
 
-	var filter = function(proc) {
-		return !id || proc.id === id;
-	};
+  var filter = function(proc) {
+    return !id || proc.id === id;
+  };
 
-	c.ps(function(err, docks) {
-		if (err) return ui.error(err);
+  c.ps(function(err, docks) {
+    if (err) return ui.error(err);
     var found = 0;
     
-		docks.forEach(function(dock) {
-			var status = dock.list.filter(filter).map(function(proc) {
-				return proc.status;
-			});
+    docks.forEach(function(dock) {
+      var status = dock.list.filter(filter).map(function(proc) {
+        return proc.status;
+      });
 
       found += status.length
 
-			status.forEach(function(st) {
-				if (st === 'crashed') process.exit(1)
-			})
-		});
+      status.forEach(function(st) {
+        if (st === 'crashed') process.exit(1)
+      })
+    });
 
     if (!found) process.exit(1)
-	});
+  });
 };
